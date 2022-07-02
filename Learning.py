7b796fd21039034268b472277db963819c7fa5de
Lucas <lucashart628@gmail.com>
2022-07-01 17:44:59 -0700

    Lists
    Gone Through The List Course For Python3 On Codecademy

--- Learning.py
+++ Learning.py
@@ -241,7 +241,7 @@ print("You are going to " + planet + ", which has a weight of " + str(weight))
 # Sal's Shipping
 
 # Weight
-weight = input("How much does your package weigh? ")
+weight = 2
 
 # Ground Shipping
 ground_price = 20
@@ -271,3 +271,115 @@ else:
 print("Drone:", drone_price)
 print("Ground:", ground_price)
 print("Ground Premium:", ground_premium_price)
+
+# Lists
+
+heights = [61, 70, 67, 64, 65]
+
+#broken_heights = [65 71 59 62]
+
+ints_and_strings = [1, 2, 3, "four", "five", "ints_and_strings"]
+
+sam_height_and_testscore = ["Sam", 67, 85.5, True]
+
+#empty lists can be used if you plan to fill it up later
+my_empty_list = []
+
+example_list = [1, 2, 3, 4]
+
+#Using Append
+example_list.append(5)
+# print(example_list)
+
+#Using Remove
+example_list.remove(5)
+# print(example_list)
+
+orders = ["daisies", "periwinkle"]
+
+print(orders)
+
+orders.append("tulips")
+orders.append("roses")
+print(orders)
+
+orders = ["daisy", "buttercup", "snapdragon", "gardenia", "lily"]
+
+# Create new orders here:
+new_orders = ["lilac", "iris"]
+orders_combined = orders + new_orders
+
+print(new_orders)
+
+#broken_prices = [5, 3, 4, 5, 4] + 4
+
+employees = ["Michael", "Dwight", "Jim", "Pam", "Ryan", "Andy", "Robert"]
+
+employee_four = employees[3]
+print(employees[5])
+
+#Negative indexing
+
+shopping_list = ["eggs", "butter", "milk", "cucumbers", "juice", "cereal"]
+last_element = shopping_list[-1]
+index5_element = shopping_list[5]
+
+print(last_element, index5_element)
+
+garden_waitlist = ["Jiho", "Adam", "Sonny", "Alisha"]
+garden_waitlist[0] = "Calla"
+print(garden_waitlist)
+garden_waitlist[-1] = "Alex"
+print(garden_waitlist)
+
+# Your code below: 
+order_list = ["Celery", "Orange Juice", "Orange", "Flatbread"]
+print(order_list)
+order_list.remove("Flatbread")
+print(order_list)
+new_store_order_list = ["Orange", "Apple", "Mango", "Broccoli", "Mango"]
+print(new_store_order_list)
+new_store_order_list.remove("Mango")
+print(new_store_order_list)
+#new_store_order_list.remove("Onions")
+
+#A 2d list with three lists in each of the indices. 
+tic_tac_toe = [
+            ["X","O","X"], 
+            ["O","X","O"], 
+            ["O","O","X"]
+]
+heights = [["Jenny", 61], ["Alexus", 70], ["Sam", 67], ["Grace", 64], ["Vik", 68]]
+ages = [["Aaron", 15], ["Dhruti", 16]]
+
+#2D Lists (Nested Arrays)
+class_name_test = [["Jenny", 90], ["Alexus", 85.5], ["Sam", 83], ["Ellie", 101.5]]
+print(class_name_test)
+sams_score = class_name_test[2][1]
+print(sams_score)
+ellies_score = class_name_test[-1][-1]
+print(ellies_score)
+
+#Editing 2D Lists
+incoming_class = [["Kenny", "American", 9], ["Tanya", "Russian", 9], ["Madison", "Indian", 7]]
+print(incoming_class)
+incoming_class[2][2] = 8
+print(incoming_class)
+incoming_class[-3][-3] = "Ken"
+print(incoming_class)
+
+#Practice
+first_names = ["Ainsley", "Ben", "Chani", "Depak"]
+preferred_size = ["Small", "Large", "Medium"]
+preferred_size.append("Medium")
+print(preferred_size)
+customer_data = [["Ainsley", "Small", True], ["Ben", "Large", False], ["Chani", "Medium", True], ["Depak", "Medium", False]]
+print(customer_data)
+customer_data[2][-1] = False
+print(customer_data)
+customer_data[1].remove(False)
+customer_data_final = customer_data + [["Amit", "Large", True], ["Karim", "X-Large", False]]
+print(customer_data_final)
+
+print('''
+End Of Code''')

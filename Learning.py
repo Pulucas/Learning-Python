dd8406d3f8e19c6025d98fc643199c3cf3de52d6
Lucas <40408423+Pulucas@users.noreply.github.com>
2022-09-09 11:48:40 -0400

    Zip function

--- Learning.py
+++ Learning.py
@@ -518,4 +518,15 @@ example_tuple = (4,)
 print(example_tuple)
 
 print('''
+''')
+
+# Using The Zip Function
+owners = ["Jenny", "Alexus", "Sam", "Grace"]
+dogs_names = ["Elphonse", "Dr. Doggy DDS", "Carter", "Ralph"]
+names_and_dogs_names = zip(owners, dogs_names)
+print(names_and_dogs_names)
+list_of_names_and_dogs_names = list(names_and_dogs_names)
+print(list_of_names_and_dogs_names)
+
+print('''
 End Of Code''')
